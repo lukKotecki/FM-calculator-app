df110cfd90c0af58ea25ebcc14beac7dec30e805
Lukas <lukkotecki@gmail.com>
2024-02-14 00:08:34 +0100

    style configuration

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,26 @@
+import React from 'react'
 import Display from './components/Display'
 import Header from './components/Header'
 
+export const ThemeSelectorContext = React.createContext({
+  themeName:'dark'
+})
+
 function App() {
   
+  const [theme, setTheme ] = React.useState('dark')
+  const switchTheme = (newTheme) => setTheme(newTheme)
+
+  console.log(theme)
 
   return (
-    <main className='main-container'>
-      <Header />
-      <Display />
-     to jest tekst
-    </main>
+    <ThemeSelectorContext.Provider value={{themeName:'dark', setTheme}}>
+      <main className='main-container'>
+        <Header />
+        <Display />
+      to jest tekst
+      </main>
+    </ThemeSelectorContext.Provider>
   )
 }
 
--- src/components/ThreeStateToggle/ThreeStateToggle.jsx
+++ src/components/ThreeStateToggle/ThreeStateToggle.jsx
@@ -1,47 +1,50 @@
-import React from 'react'
+import React, { useCallback, useContext } from 'react'
 import './ThreeStateToggle.css'
+import { ThemeSelectorContext } from '../../App'
 
 export default function ThreeStateToggle(){
+    const {themeName, setTheme} = useContext(ThemeSelectorContext)
+    const [style, setStyle ] = React.useState('dark')
 
-    console.clear()
+    const dark = React.useRef(null)
+    const light = React.useRef(null)
+    const violet = React.useRef(null)
 
-    function handleClick(e){
-        console.log(e.target.id)
+    // console.log(themeName)
 
-        switch(e.target.id){
-            case 'one':
-                e.target.nextElementSibling.style.opacity = 0
-                e.target.nextElementSibling.nextElementSibling.style.opacity = 0
-                console.log('opcja pierwsza')
+    React.useEffect(()=> {
+        dark.current.style.opacity = 0
+        light.current.style.opacity = 0
+        violet.current.style.opacity = 0
+        setTheme(style)
+        
+        switch(style){
+            case 'dark':
+            dark.current.style.opacity = 1;
                 break;
-            case 'two':
-                e.target.previousElementSibling.style.opacity = 0
-                e.target.nextElementSibling.style.opacity = 0
-                console.log('opcja druga')
+            case 'light':
+            light.current.style.opacity = 1
                 break;
-            case 'three':
-                e.target.previousElementSibling.style.opacity = 0
-                e.target.previousElementSibling.previousElementSibling.style.opacity = 0
-                console.log('opcja trzecaia')
+            case 'violet':
+            violet.current.style.opacity = 1
                 break;
             default:
-                console.log('opcja nieznana')
+                console.log('unknown option')
         }
-        
+    }, [style])
 
-        // e.target.previousElementSibling.style.opacity = 0
-        e.target.style.opacity = 1
-
+    function handleClick(e){
+        setStyle(e.target.id)
     }
 
 
     return(
         <div className='toggle'>
                     
-            <div className="tri-state-toggle" onClick={handleClick}>
-                <input className="button" type="radio" name="toggle" id="one" />
-                <input className="button" type="radio" name="toggle" id="two" />
-                <input className="button" type="radio" name="toggle" id="three" />
+            <div className="tri-state-toggle">
+                <input ref={dark} onClick={handleClick} className="button" type="radio" name="toggle" id="dark" />
+                <input ref={light} onClick={handleClick} className="button" type="radio" name="toggle" id="light" />
+                <input ref={violet} onClick={handleClick} className="button" type="radio" name="toggle" id="violet" />
             </div>
 
 
